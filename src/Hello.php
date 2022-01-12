e950be3e865519b8d26681d83b95773c08fd4ed3
Imdad <imdad@digitalhotelier.com>
2022-01-12 22:05:51 +0530

    Added hello class

---
+++ src/Hello.php
@@ -0,0 +1,18 @@
+<?php 
+namespace Imdad\ComposerPkgTest;
+
+class Hello
+{
+
+    /**
+     * Just a do something function
+     *
+     * @return integer
+     */
+    public function doSomething():int 
+    {
+        $x= 1;
+        $x++;
+        return $x;
+    }
+}
